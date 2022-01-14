dc40ac00c4f1b6e709cd1fbeece9a9e4b12f46f8
Bohdan Protas <bo7pro@gmail.com>
2022-01-14 14:23:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#  SwiftUI learning repository
+
+created this repository to track my progress in SwiftUI learning from the Apple app dev training resources
+https://developer.apple.com/tutorials/app-dev-training/
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by protas on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
