78af3d0504af2b9334490eb874e5c3c22323332b
Bohdan Protas <bo7pro@gmail.com>
2022-01-14 16:01:42 +0200

    [Scrumdinger] - add meeting view

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by protas on 14.01.2022.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
