c28da9b863a50f5645aeba86b9be7d9773d6eee1
Bohdan Protas <bo7pro@gmail.com>
2022-01-17 23:24:18 +0200

    [Scrumdinger] Card view accessibility improvements

--- Scrumdinger/Scrumdinger/CardView.swift
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -11,16 +11,20 @@ struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
         VStack(alignment: .leading) {
-            Text(scrum.title).font(.headline)
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock").labelStyle(.trailingIcon)
             }
             .font(.caption)
-            .foregroundColor(scrum.theme.accentColor)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
